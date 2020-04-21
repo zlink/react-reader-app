92fbb8a86120206293fc23c08a62f90f67af9723
zlink <djyzzc@gmail.com>
2020-04-21 23:48:56 +0800

    remove css

--- src/store/index.js
+++ src/store/index.js
@@ -7,5 +7,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
   reducers,
+  {},
   composeEnhancers(applyMiddleware(thunk, loger)),
 );
